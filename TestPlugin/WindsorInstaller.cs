dfc53af3783f554f341edffc661ef6f92901062d
Leandro Gois Sousa <leandro.sousa@titulo.net>
2016-05-25 17:59:11 -0300

    Implementação do Castle.

---
+++ TestPlugin/WindsorInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace TestPlugin
+{
+    public class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromAssemblyInDirectory(
+                    new AssemblyFilter(@"C:\SRC\Tests\TestPlugin\TestPlugin.Concrete\bin\Debug", "TestPlugin.Concrete.dll"))
+                    .Pick()
+                    .WithServiceAllInterfaces()
+                    .LifestyleTransient());
+
+            container.Register(Classes.FromThisAssembly()
+                .Pick()
+                .WithServiceAllInterfaces()
+                .LifestyleTransient());
+        }
+    }
+}
